f3761f35e40d9ddab751e1d87517b75728f07daa
Robert Hönig <robhoenig@gmail.com>
2018-12-26 22:42:30 +0100

    Add the output generation part of the algorithm (UNTESTED).

--- src/load.jl
+++ src/load.jl
@@ -1,20 +1,19 @@
-# using Images
 using FileIO
+using StatsBase
 
-input = []
+input = []  # Array{RGB{Normed{UInt8,8}},2}
 patternToId = Dict()
 idToPattern = Dict()
-patternCount = Dict()
-patternAdjacency = Dict()
+patternCount = Dict()  # patternId => Int
+patternAdjacency = Dict()  # Set([(direction, patternId1, patternId2), ...])
 
 const directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
 
 function load(;
     filename,
     patternsize=2,
-    width=64,  # Output.
-    height=64, # Output.
+    width=16,  # Width of output image in number of patterns.
+    height=16, # Height of output image in number of patterns.
     periodicInput=false,
     periodicOutput=false)
     
@@ -27,8 +26,6 @@ function load(;
     begin  # Generate patterns.
     global input = FileIO.load(filename)
     (input_height, input_width) = size(input)
-    
-
     # TODO: Account for periodicInput.
     for col in 1:input_width+1-patternsize, row in 1:input_height+1-patternsize
         pattern = input[row:row+patternsize-1, col:col+patternsize-1]
@@ -53,10 +50,67 @@ function load(;
         lap2 = idToPattern[p2][(dy==-1 ? 2 : 1):(dy==1 ? end-1 : end), (dx==-1 ? 2 : 1):(dx==1 ? end-1 : end)]
         overlap = lap1 == lap2
         if overlap
-            adjacency = get!(patternAdjacency, direction, Dict())
-            adjacent = get!(adjacency, p1, Set())
-            push!(adjacent, p2)
+            push!(patternAdjacency, (direction, p1, p2))
         end
     end
     end
+
+    begin  # Generate output.
+        field_patterns = [Set(keys(idToPattern)) for i in 1:height, j in 1:width]
+        
+        while !all(x -> size(x) == 1, field_patterns)
+            if any(x -> size(x) == 0, field_patterns)
+                error("Impossible to complete generation. Restart the algorithm.")
+            end
+            # Find field with lowest entropy in field_patterns.
+            weight_sum = sum(field_patterns) do valid_patterns
+                sum(id -> patternCount[id], valid_patterns)
+            end
+            field_entropies = map(field_patterns) do valid_patterns
+                # Ignore already collapsed fields.
+                if length(valid_patterns) <= 1
+                    return 10e9
+                end
+                randn()*1e-6 - sum(valid_patterns) do id
+                    weight = patternCount[id]
+                    weight * log(weight/weight_sum)
+                end
+            end
+            (_, idx_min) = findmin(field_entropies)
+            field = collect(field_patterns[idx_min])
+            choice = sample(field, getindex.(Ref(patternCount), field))
+            field_patterns[idx_min] = Set([choice])
+
+            # Enforce new constraint in all other fields.
+            field_idxs = []
+            function neighbors(idx)
+                (idx+CartesianIndex(direction) for direction in directions if checkbounds(Bool, field_patterns, idx+CartesianIndex(direction)))
+            end
+            for neighbor in neighbors(idx)
+                push!(field_idx, neighbor)
+            end
+            while !empty(field_idxs)
+                field_idx = pop!(field_idxs)
+                constrained_valid_patterns = Set([])
+                for id in field_patterns[field_idx]
+                    if all(neighbors(field_idx)) do constraint_idx
+                        any(Base.Iterators.product(field_patterns[field_idx], field_patterns[constraint_idx])) do (field_id, constraint_id)
+                            (direction, field_id, constraint_id) in patternAdjacency
+                        end
+                    end
+                        push!(constrained_valid_patterns, id)
+                    end
+                end
+                if length(constrained_valid_patterns) != length(field_patterns[field_idx])
+                    for neighbor in neighbors(field_idx)
+                        push!(field_idxs, idx)
+                    end
+                end
+                field_patterns[field_idx] = constrained_valid_patterns
+            end
+        end
+    end
+    output_patterns = map(x -> idToPattern[first(x)], field_patterns)
+    output = vcat([hcat(output_patterns[row, :]...) for row in 1:height]...)
+    output
 end
