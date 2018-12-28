537393a567214de7e70f3947c92720f72a716469
Robert Hönig <robhoenig@gmail.com>
2018-12-28 14:41:02 +0100

    Rearrange code.

--- src/overlapping_model.jl
+++ src/overlapping_model.jl
@@ -2,38 +2,7 @@ using FileIO
 using Random
 using StatsBase
 
-input = []  # Array{RGB{Float32},2}
-patternToId = Dict()
-idToPattern = Dict()
-patternCount = Dict()  # patternId => Int
-patternAdjacency = Dict()  # direction => pattern1 => Set([patterns...])
-patternsAllowed = Dict()  # direction => [width x height x patterns]
-#  pattern i at (x, y) overlaps with patterns from the field at (x, y)+direction patternsAllowed[direction][x, y, i] times.
-
-const directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-const opposite = Dict((1,0) => (-1,0), (0,1) => (0, -1), (-1, 0) => (1, 0), (0, -1) => (0, 1))
-
-function get_image(field_patterns; average_superpositions=false)
-    output_patterns = map(field_patterns) do pattern_ids
-        if average_superpositions
-            error("Superposition averaging not implemented!")
-            # sum(getindex.(Ref(idToPattern), collect(pattern_ids))) ./ length(pattern_ids)
-        else
-            pattern = length(pattern_ids) == 1 ? idToPattern[first(pattern_ids)] : zero(idToPattern[first(pattern_ids)])
-            fill(pattern[1], 1, 1)
-        end
-    end
-    vcat([hcat(output_patterns[row, :]...) for row in 1:size(output_patterns)[1]]...)
-end
-
-function print_field_patterns(field_patterns)
-    println("field_patterns:")
-    for idx in CartesianIndices(field_patterns)
-        println("$idx: $(sort(collect(field_patterns[idx])))")
-    end
-end
-
-function load(;
+function generate(;
     filename,
     patternsize=2,
     width=16,  # Width of output image in number of patterns.
@@ -42,11 +11,27 @@ function load(;
     periodicOutput=false,
     seed=0)
     
-    global input = []
-    global patternToId = Dict()
-    global idToPattern = Dict()
-    global patternCount = Dict()
-    global patternAdjacency = Dict()
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    opposite = Dict((1,0) => (-1,0), (0,1) => (0, -1), (-1, 0) => (1, 0), (0, -1) => (0, 1))
+    input = []  # Array{RGB{Float32},2}
+    patternToId = Dict()
+    idToPattern = Dict()
+    patternCount = Dict()  # patternId => Int
+    patternAdjacency = Dict()  # direction => pattern1 => Set([patterns...])
+    patternsAllowed = Dict()  # direction => [width x height x patterns]
+    #  pattern i at (x, y) overlaps with patterns from the field at (x, y)+direction patternsAllowed[direction][x, y, i] times.
+
+    function get_image(field_patterns; average_superpositions=false)
+        output_patterns = map(field_patterns) do pattern_ids
+            if average_superpositions
+                error("Superposition averaging not implemented!")
+            else
+                pattern = length(pattern_ids) == 1 ? idToPattern[first(pattern_ids)] : zero(idToPattern[first(pattern_ids)])
+                fill(pattern[1], 1, 1)
+            end
+        end
+        vcat([hcat(output_patterns[row, :]...) for row in 1:size(output_patterns)[1]]...)
+    end
 
     if seed != 0
         Random.seed!(seed)
@@ -73,8 +58,6 @@ function load(;
         end
     end
     end
-
-    @assert all([patternToId[idToPattern[i]] == i for i in 1:length(patternToId)])
 
     begin  # Initialize patternAdjacency.
     for direction in directions, p1 in keys(idToPattern), p2 in keys(idToPattern)
@@ -113,12 +96,9 @@ function load(;
             end
         end
         while !all(x -> length(x) == 1, field_patterns)
-            # print_field_patterns(field_patterns)
-            # flush(stdout)
             if any(x -> length(x) == 0, field_patterns)
                 error("Impossible to complete generation. Restart the algorithm.")
             end
-            # display(get_image(field_patterns, average_superpositions=false))
             # Find field with lowest entropy in field_patterns.
             weight_sum = sum(field_patterns) do valid_patterns
                 sum(id -> patternCount[id], valid_patterns)
@@ -143,8 +123,6 @@ function load(;
                 disallowPattern(idx_min, pattern_id)
             end
             field_patterns[idx_min] = Set([choice])
-            # println("Choosing pattern $choice")
-            # println("finished field selection")
             flush(stdout)
             # Enforce new constraint in all other fields.
             field_idxs = []
@@ -157,7 +135,6 @@ function load(;
                 for pattern_id in field_patterns[field_idx]
                     if all(neighbors(field_idx)) do (_, direction)
                             patternsAllowed[direction][field_idx[1], field_idx[2], pattern_id] > 0
-                            # println("comparing field $field_idx with $constraint_idx")
                         end
                         push!(constrained_valid_patterns, pattern_id)
                     else
@@ -170,12 +147,8 @@ function load(;
                     end
                 end
                 field_patterns[field_idx] = constrained_valid_patterns
-                # println("constraint enforcing")
-                # print_field_patterns(field_patterns)
             end
         end
     end
-    # print_field_patterns(field_patterns)
-    println("finished")
     get_image(field_patterns)
 end
